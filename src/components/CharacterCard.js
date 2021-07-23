acd65d5739fda2ef63bc362f1d1cd6391c4ab253
hellodebs <debora.behrens@outlook.com>
2021-07-23 14:35:15 +0200

    Change naming from main, fixing small css bug

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,7 +2,7 @@ import "./CharacterCard.css";
 
 export default function CharacterCard() {
   return (
-    <main className="app__main">
+    <main className="app__character-card">
       <div>CharacterCard</div> <div>CharacterCard</div> <div>CharacterCard</div>
       <div>CharacterCard</div>
     </main>

988f3bd45ab743bca15c2275bd5a5015e7558489
hellodebs <debora.behrens@outlook.com>
2021-07-23 11:20:35 +0200

    Clean up cd code, add pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import "./App.css";
+// import AllCharacters from "./pages/AllCharacters";
+// import OneCharacter from "./pages/OneCharacter";
+// import Home from "./pages/Home";
 
 function App() {
   return <div className="App">Rick & Morty</div>;

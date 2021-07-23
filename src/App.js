9a50469c338169cb09612052ab9bd40c41bd2a3e
hellodebs <debora.behrens@outlook.com>
2021-07-23 14:12:05 +0200

    Add LoadMore.js for button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import CharacterCard from "./components/CharacterCard";
+import LoadMore from "./components/LoadMore";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <CharacterCard />
+      <LoadMore />
     </div>
   );
 }

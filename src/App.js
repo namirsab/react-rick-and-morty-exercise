f2531236ddcd800a2922989155133e21b9955ffd
John Goehner <john.goehner@gmx.de>
2021-07-23 11:09:14 +0200

    Setup react-router-dom + simple layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { NavLink, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App">Rick & Morty</div>;
+  return (
+    <div className="App">
+      <header className="Header">
+        <p>R&M LOGO</p>
+        <nav className="Header__navigation">
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+          <NavLink to="/characters">Characters</NavLink>
+        </nav>
+      </header>
+      <main className="Main">
+        <Switch>
+          <Route path="/characters/:id">Single Character</Route>
+          <Route path="/characters">Characters Page</Route>
+          <Route path="/">Home</Route>
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;

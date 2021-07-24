002559a57c62e955619a294ec1fbac2fcbd99fa8
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-07-24 22:02:35 +0200

    somehow add filter for multiple favourites and everything works

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,3 +1,42 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
 export default function Favourites() {
-  return <h1>Favourites</h1>;
+  const { favourites } = useContext(GlobalContext);
+
+  return (
+    <div>
+      <h1>Favourites</h1>
+      {favourites
+        .filter(
+          (favourites, index, self) =>
+            index ===
+            self.findIndex(
+              (t) => t.id === favourites.id && t.name === favourites.name
+            )
+        )
+        .map((char) => (
+          <div className="singleChar__card">
+            <h2>{char.name}</h2>
+            <img src={char.image} alt={`${char.name} avatar`} />
+            <div className="singleChar__textBox">
+              <p
+                className={`characters__status${
+                  char.status === "Alive"
+                    ? " alive"
+                    : char.status === "Dead"
+                    ? " dead"
+                    : " unknown"
+                }`}
+              >
+                Status: {char.status}
+              </p>
+              <p>Species: {char.species}</p>
+              <p>Gender: {char.gender}</p>
+              <p>Origin: {char.origin.name}</p>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
